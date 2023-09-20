0958b2d0e0e6866153ea5ea64cdffe815954bac6
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-09-20 14:15:06 +0200

    Update MainWindowViewModel.cs

--- wcli/ViewModels/MainWindowViewModel.cs
+++ wcli/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
 using System.Threading;
@@ -17,15 +18,36 @@ public partial class MainWindowViewModel : ViewModelBase
     [ObservableProperty] private string? _walletName;
     [ObservableProperty] private string? _walletPassword;
     [ObservableProperty] private object? _currentDialog;
+    [ObservableProperty] private ObservableCollection<RpcMethodViewModel>? _rpcMethods;
 
     public MainWindowViewModel()
     {
         RpcServerPrefix = "http://127.0.0.1:37128";
         WalletName = "Wallet 1";
         WalletPassword = "";
+        RpcMethods = new ObservableCollection<RpcMethodViewModel>()
+        {
+            new ("GetStatus", GetStatusCommand),
+            new ("CreateWallet", CreateWalletCommand),
+            new ("ListCoins", ListCoinsCommand),
+            new ("ListUnspentCoins", ListUnspentCoinsCommand),
+            new ("GetWalletInfo", GetWalletInfoCommand),
+            new ("GetNewAddress", GetNewAddressCommand),
+            new ("Send", SendCommand),
+            new ("Build", BuildCommand),
+            new ("Broadcast", BroadcastCommand),
+            new ("GetHistory", GetHistoryCommand),
+            new ("ListKeys", ListKeysCommand),
+            new ("StartCoinJoin", StartCoinJoinCommand),
+            new ("StopCoinJoin", StopCoinJoinCommand),
+            new ("Stop", StopCommand)
+        };
     }
 
-    private async Task<Rpc?> SendRpcMethod<T>(RpcMethod requestBody, string rpcServerUri, JsonTypeInfo<T> jsonTypeInfo) where T: Rpc
+    private async Task<Rpc?> SendRpcMethod<T>(
+        RpcMethod requestBody, 
+        string rpcServerUri, 
+        JsonTypeInfo<T> jsonTypeInfo) where T: Rpc
     {
         var requestBodyJson = JsonSerializer.Serialize(requestBody, RpcJsonContext.Default.RpcMethod);
         var cts = new CancellationTokenSource();
@@ -88,28 +110,6 @@ public partial class MainWindowViewModel : ViewModelBase
     }
 
     [RelayCommand]
-    private async Task GetWalletInfo()
-    {
-        // {"jsonrpc":"2.0","id":"1","method":"getwalletinfo"}
-        var requestBody = new RpcMethod()
-        {
-            Method = "getwalletinfo"
-        };
-        var rpcServerUri = $"{RpcServerPrefix}/{WalletName}";
-        var rpcResult = await SendRpcMethod(requestBody, rpcServerUri, RpcJsonContext.Default.RpcGetWalletInfoResult);
-        if (rpcResult is RpcGetWalletInfoResult { Result: not null } rpcGetWalletInfoResult)
-        {
-            // TODO:
-            CurrentDialog = rpcGetWalletInfoResult.Result;
-        }
-        else if (rpcResult is RpcErrorResult { Error: not null } rpcErrorResult)
-        {
-            // TODO:
-            CurrentDialog = rpcErrorResult.Error;
-        }
-    }
-
-    [RelayCommand]
     private async Task CreateWallet()
     {
         // {"jsonrpc":"2.0","id":"1","method":"createwallet","params":["WalletName", "UserPassword"]}'
@@ -134,5 +134,93 @@ public partial class MainWindowViewModel : ViewModelBase
             // TODO:
             CurrentDialog = rpcErrorResult.Error;
         }
+    }
+
+    [RelayCommand]
+    private async Task ListCoins()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task ListUnspentCoins()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task GetWalletInfo()
+    {
+        // {"jsonrpc":"2.0","id":"1","method":"getwalletinfo"}
+        var requestBody = new RpcMethod()
+        {
+            Method = "getwalletinfo"
+        };
+        var rpcServerUri = $"{RpcServerPrefix}/{WalletName}";
+        var rpcResult = await SendRpcMethod(requestBody, rpcServerUri, RpcJsonContext.Default.RpcGetWalletInfoResult);
+        if (rpcResult is RpcGetWalletInfoResult { Result: not null } rpcGetWalletInfoResult)
+        {
+            // TODO:
+            CurrentDialog = rpcGetWalletInfoResult.Result;
+        }
+        else if (rpcResult is RpcErrorResult { Error: not null } rpcErrorResult)
+        {
+            // TODO:
+            CurrentDialog = rpcErrorResult.Error;
+        }
+    }
+
+    [RelayCommand]
+    private async Task GetNewAddress()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task Send()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task Build()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task Broadcast()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task GetHistory()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task ListKeys()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task StartCoinJoin()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task StopCoinJoin()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    private async Task Stop()
+    {
+        throw new NotImplementedException();
     }
 }
