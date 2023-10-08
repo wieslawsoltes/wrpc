193bd86f721b405fea27612c9eecaabaa894921c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-10-08 14:19:10 +0200

    Add views

---
+++ wcli/Views/Methods/ExcludeFromCoinjoinView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace WasabiCli.Views.Methods;
+
+public partial class ExcludeFromCoinjoinView : UserControl
+{
+    public ExcludeFromCoinjoinView()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ wcli/Views/Methods/SpeedUpTransactionView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace WasabiCli.Views.Methods;
+
+public partial class SpeedUpTransactionView : UserControl
+{
+    public SpeedUpTransactionView()
+    {
+        InitializeComponent();
+    }
+}
+
