24f738d223804574eca24e128d05929bb44a0544
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-02-12 20:37:13 +0100

    Create RpcListPaymentsInCoinjoinResult.cs

---
+++ wrpc.core/Models/Rpc/Results/RpcListPaymentsInCoinjoinResult.cs
@@ -0,0 +1,10 @@
+using System.Text.Json.Serialization;
+
+namespace WasabiRpc.Models.Rpc.Results;
+
+public class RpcListPaymentsInCoinjoinResult : Rpc
+{
+    [JsonPropertyName("result")]
+    [JsonRequired]
+    public string? Result { get; set; }
+}
