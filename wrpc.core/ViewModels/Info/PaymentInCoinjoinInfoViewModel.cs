a25da1fb2c252a94c5bb968e34c9663d5d904761
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-02-13 07:20:54 +0100

    Create PaymentInCoinjoinInfoViewModel.cs

---
+++ wrpc.core/ViewModels/Info/PaymentInCoinjoinInfoViewModel.cs
@@ -0,0 +1,30 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using WasabiRpc.Models.Services;
+
+namespace WasabiRpc.ViewModels.Info;
+
+public partial class PaymentInCoinjoinInfoViewModel : RoutableViewModel
+{
+    [ObservableProperty] 
+    private string? _id;
+
+    [ObservableProperty] 
+    private long _amount;
+
+    [ObservableProperty] 
+    private string? _destination;
+
+    [ObservableProperty] 
+    private PaymentInCoinjoinStateInfoViewModel? _state;
+
+    [ObservableProperty] 
+    private string? _address;
+
+    public PaymentInCoinjoinInfoViewModel(
+        IRpcServiceViewModel rpcService, 
+        INavigationService navigationService,
+        INavigationService detailsNavigationService)
+        : base(rpcService, navigationService, detailsNavigationService)
+    {
+    }
+}
