4ea6131bb002b9f534dee6961cf5a8ebe836ef1f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-03-08 00:05:28 +0100

    Update RpcCancelPaymentInCoinjoinResult.cs

--- wrpc.core/Models/Rpc/Results/RpcCancelPaymentInCoinjoinResult.cs
+++ wrpc.core/Models/Rpc/Results/RpcCancelPaymentInCoinjoinResult.cs
@@ -1,10 +1,5 @@
-using System.Text.Json.Serialization;
-
 namespace WasabiRpc.Models.Rpc.Results;
 
 public class RpcCancelPaymentInCoinjoinResult : Rpc
 {
-    [JsonPropertyName("result")]
-    [JsonRequired]
-    public string? Result { get; set; }
 }
